d73dc68181466b512d4888cac826bf52c14a9005
kwang-hoonkang <gniusk614@nate.com>
2026-03-31 21:19:07 +0900

    comfort

---
+++ js/tattoo-subpage.js
@@ -0,0 +1,34 @@
+$(function () {
+  var site = window.ComfortSite || {};
+  var modules = site.modules || {};
+  var siteMap = site.siteMap || {};
+
+  if (typeof modules.initHeaderMenu === "function") {
+    modules.initHeaderMenu({
+      headerArea: $(".header-area"),
+      menuButton: $(".btn-menu"),
+      mobileHome: $(".header-mobile-home"),
+      gnbItems: $(".gnb-item"),
+      quickMenu: $("#headerQuickMenu"),
+      mobileMenuBackdrop: $("#mobileMenuBackdrop"),
+      mobileMenuPanel: $("#mobileMenuPanel"),
+      mobileMenuGroups: $("#mobileMenuGroups"),
+      navigationMenus: siteMap.navigationMenus,
+      defaultMenuKey: "tattoo"
+    });
+  }
+
+  $("[data-scroll-top]").on("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+
+  if (typeof modules.initScrollReveal === "function") {
+    modules.initScrollReveal({
+      root: document,
+      selector: "[data-reveal]"
+    });
+  }
+});
